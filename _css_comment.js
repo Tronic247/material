003818e31e0341d2cce1b0d2fdaae3df7b0021a4
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-01 15:12:30 +0530

    Update _css_comment.js

--- _css_comment.js
+++ _css_comment.js
@@ -15,5 +15,5 @@ var buffer = new Buffer(
 
 fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
 fs.writeSync(fd, data, 0, data.length, buffer.length); //append old data
-// or fs.appendFile(fd, data);
+fs.appendFileSync(file, '/*# sourceMappingURL=material.css.map */');
 fs.close(fd);
