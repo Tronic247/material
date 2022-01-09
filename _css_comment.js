effb9d95dc5842cfe399c2c9af6a43bf8ed447ff
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-09 19:29:46 +0530

    Update _css_comment.js

--- _css_comment.js
+++ _css_comment.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const file = "dist/material.min.css";
 const data = fs.readFileSync(file); 
 const fd = fs.openSync(file, 'w+');
+
+const file2 = "dist/material.min.js";
+const data2 = fs.readFileSync(file2); 
+const fd2 = fs.openSync(file2, 'w+');
+
 const buffer = new Buffer(
  `/*
  * Tronic247 Material design - v3.0
@@ -17,3 +22,8 @@ fs.writeSync(fd, buffer, 0, buffer.length, 0);
 fs.writeSync(fd, data, 0, data.length, buffer.length); 
 fs.appendFileSync(file, "\n \n /*# sourceMappingURL=material.css.map */");
 fs.close(fd);
+
+fs.writeSync(fd2, buffer, 0, buffer.length, 0);
+fs.writeSync(fd2, data2, 0, data2.length, buffer.length); 
+fs.appendFileSync(file2, "\n \n /*# sourceMappingURL=material.css.map */");
+fs.close(fd2);
