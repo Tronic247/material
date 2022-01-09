4d612e5c3b4e5988f8ec2a32723ce22ad0dca7d7
Posandu Mapa <76736580+Posandu@users.noreply.github.com>
2022-01-09 20:00:51 +0530

    Update _purge.js

--- _purge.js
+++ _purge.js
@@ -5,7 +5,7 @@ const files = ["material.min.css", "material.min.js", "material.css"];
 files.forEach((file) => {
     https.get(`https://cdn.jsdelivr.net/npm/@material/material@${file}`, (res) => {
         res.setEncoding("utf8");
-        let body = {};
+        let body = '';
         res.on("data", (data) => {
             const r = data;
             body += `${file.toString()}`;
