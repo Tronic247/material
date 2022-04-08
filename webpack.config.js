0592fbd72151ac0c2b12be2055cea10639841cc9
Posandu Mapa <posandumapa@gmail.com>
2022-04-08 10:02:30 +0530

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 const path = require("path");
 const webpack = require("webpack");
+const ClosurePlugin = require("closure-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/js/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "material.min.js",
-    library: "Material",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
-  mode: "production",
-  plugins: [
-    new webpack.BannerPlugin(`
+	entry: path.resolve(__dirname, "src/js/index.js"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "material.min.js",
+		library: "Material",
+		libraryTarget: "umd",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+		],
+	},
+	mode: "production",
+	plugins: [
+		new webpack.BannerPlugin(`
          Tronic247 Material design - v3.0
          https://material.tronic247.com
         
@@ -28,13 +29,14 @@ module.exports = {
          licensed under the MIT license.
          https://github.com/Tronic247/material/blob/Main/LICENSE
          `),
-  ],
-  optimization: {
-    minimizer: [
-      (compiler) => {
-        const TerserPlugin = require("terser-webpack-plugin");
-        new TerserPlugin().apply(compiler);
-      },
-    ],
-  },
+	],
+	optimization: {
+		minimizer: [
+			(compiler) => {
+				const TerserPlugin = require("terser-webpack-plugin");
+				new TerserPlugin().apply(compiler);
+			},
+			new ClosurePlugin({ mode: "STANDARD" }),
+		],
+	},
 };
