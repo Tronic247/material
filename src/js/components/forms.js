7a13632fcf732d048f8b25c3c996c44740208b05
Posandu Mapa <posandumapa@gmail.com>
2022-09-07 20:02:22 +0530

    Add textarea

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -1,10 +1,56 @@
-import { $$, $addClass, $element, $hasClass } from "../element";
+import {
+	$,
+	$$,
+	$addClass,
+	$addEvents,
+	$element,
+	$hasClass,
+	$removeClass,
+} from "../element";
 
-function Forms() {
-	
+class Forms {
+	init() {
+		const forms = $$(".textbox");
 
+		forms.forEach((form) => {
+			const input = $(".input", form);
+
+			if (input.value) {
+				$addClass(form, "floating");
+			} else {
+				$removeClass(form, "floating");
+			}
+
+			$addEvents(form, "click", () => {
+				$addClass(form, "floating");
+				input.focus();
+			});
+
+			$addEvents(input, "focus", () => {
+				$addClass(form, "focus");
+				$addClass(form, "floating");
+			});
+
+			$addEvents(input, "blur", () => {
+				$removeClass(form, "focus");
+
+				if (input.value) {
+					$addClass(form, "floating");
+				} else {
+					$removeClass(form, "floating");
+				}
+			});
+
+			$addEvents(input, "input", () => {
+				if (input.value) {
+					$addClass(form, "floating");
+				} else {
+					$removeClass(form, "floating");
+				}
+			});
+		});
+	}
 }
-Forms();
+new Forms().init();
 
 export default Forms;
-
